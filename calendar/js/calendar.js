55826acc51a2c4c556744335809a9c2c542eb048
zsd01082 <332006401@qq.com>
2017-09-19 16:15:59 +0800

    task 40
    天气插件，已经可以成功显示当前月份

--- calendar/js/calendar.js
+++ calendar/js/calendar.js
@@ -1,6 +1,96 @@
-class Calendar extends Table {
-    constructor(o) {
-        this.tbody = o.data.tbody
-        this.append = o.append
+class Calendar {
+    constructor(nowDate) {
+        this.dates = []
+        this.table = null
+        this.thead = null
+        this.tbody = null
+        this.nowDate = nowDate
+        this.getDates(nowDate)
+        this.highlightDate(nowDate)
+    }
+
+    getDates(date) {
+        let first = new Date(new Date(date).setDate(1)),
+            firDate = first.getDate(),
+            firDay = first.getDay()
+        let last = new Date(new Date(new Date(date).setMonth(date.getMonth() + 1)).setDate(0)),
+            lastDate = last.getDate(),
+            lastDay = last.getDay()
+
+        let dates = []
+        let tmpDay = firDay,
+            tmpDate = firDate
+        while (tmpDay > 0) {
+            dates.push('')
+            tmpDay--
+        }
+        do {
+            dates.push(tmpDate)
+            tmpDate++
+        } while (tmpDate <= lastDate)
+        tmpDay = lastDay
+        while (tmpDay < 6) {
+            dates.push('')
+            tmpDay++
+        }
+        this.dates.push(dates.slice(0, 7))
+        this.dates.push(dates.slice(7, 14))
+        this.dates.push(dates.slice(14, 21))
+        this.dates.push(dates.slice(21, 28))
+        this.dates.push(dates.slice(28, 35))
+        this.renderTable()
+    }
+
+    renderTable() {
+        let table = document.createElement('table')
+        let thead = document.createElement('thead')
+        let tbody = document.createElement('tbody')
+        this.table = table
+        this.thead = thead
+        this.tbody = tbody
+        table.appendChild(thead)
+        table.appendChild(tbody)
+        this.renderThead()
+        this.renderTbody()
+    }
+
+    renderThead() {
+        this.thead.innerHTML = ''
+        let thead = ['日', '一', '二', '三', '四', '五', '六', ]
+        for (let i = 0; i < thead.length; i++) {
+            let td = document.createElement('td')
+            td.innerHTML = thead[i]
+            this.thead.appendChild(td)
+        }
+    }
+
+    renderTbody() {
+        this.tbody.innerHTML = ''
+        for (let i = 0; i < this.dates.length; i++) {
+            let tr = document.createElement('tr')
+            this.tbody.appendChild(tr)
+            for (let j = 0; j < this.dates[i].length; j++) {
+                let td = document.createElement('td')
+                td.innerHTML = this.dates[i][j]
+                tr.appendChild(td)
+            }
+        }
+    }
+
+    highlightDate(date) {
+        let day = date.getDate()
+        for (let i = 0; i < this.dates.length; i++) {
+            let index = this.dates[i].indexOf(day)
+            if (index != -1) {
+                let column = index
+                let row = i
+                let o = this.tbody.getElementsByTagName('tr')[row].getElementsByTagName('td')[column]
+                o.style.backgroundColor = '#3f3f3f'
+            }
+        }
+    }
+
+    appendIn(e) {
+        e.appendChild(this.table)
     }
 }
--- calendar/js/main.js
+++ calendar/js/main.js
@@ -1,52 +1,11 @@
-const $ = e => document.querySelecto
+const $ = e => document.querySelector(e)
 
 const log = console.log.bind()
 
 const __main = function() {
-    /* let calendar = new Calendar({
-        append: $('body'),
-        data: {
-            thead: ['日', '一', '二', '三', '四', '五', '六', ],
-            tbody: [
-                [27, 28, 29, 30, 31, 1, 2],
-                [3, 4, 5, 6, 7, 8, 9],
-                [10, 11, 12, 13, 14, 15, 16],
-                [17, 18, 19, 20, 21, 22, 23],
-                [24, 25, 26, 27, 28, 29, 30],
-            ]
-        },
-        sortable: false,
-        forzenHead: false,
-    }) */
     let nowDate = new Date()
-    getCalendar(nowDate)
-}
-
-const getCalendar = function(date) {
-    let first = new Date(new Date(date).setDate(1)),
-        firDate = first.getDate(),
-        firDay = first.getDay()
-    let last = new Date(new Date(new Date(date).setMonth(date.getMonth() + 1)).setDate(0)),
-        lastDate = last.getDate(),
-        lastDay = last.getDay()
-
-    let result = []
-    let tmpDay = firDay,
-        tmpDate = firDate
-    while (tmpDay > 0) {
-        result.push('')
-        tmpDay--
-    }
-    do {
-        result.push(tmpDate)
-        tmpDate++
-    } while (tmpDate <= lastDate)
-    tmpDay = lastDay
-    while (tmpDay < 6) {
-        result.push('')
-        tmpDay++
-    }
-    log(result)
+    let calendar = new Calendar(nowDate)
+    calendar.appendIn($('body'))
 }
 
 __main()
